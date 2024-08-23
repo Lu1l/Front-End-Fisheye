afc8dab5a207cb00874bf53b33b74564c387294f
Lu1l <92431867+Lu1l@users.noreply.github.com>
2024-08-23 17:38:29 +0200

    fix+new erreur and link

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,43 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, price, country , city,tagline,id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+        
         const article = document.createElement( 'article' );
+        article.addEventListener('click', function() { 
+            alert('Erreur');
+          }, false);
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const h3 = document.createElement( 'h3' );
+        const pgTagline = document.createElement( 'p' );   
+        const pgPrice = document.createElement( 'p' ); 
         h2.textContent = name;
+        pgTagline.textContent = tagline;
+        pgPrice.textContent = price+" €"+"/jour";
+        pgPrice.classList.add("price");
+        h3.textContent = country+", "+city;
+        h3.classList.add("countryCity");
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(pgTagline);
+        article.appendChild(pgPrice);
         return (article);
+
+        function RedirectionJavascript(){
+
+            document.location.href= "http://photographer.html";
+            
+            }
     }
-    return { name, picture, getUserCardDOM }
+
+   
+
+
+    
+    return {getUserCardDOM}
 }
