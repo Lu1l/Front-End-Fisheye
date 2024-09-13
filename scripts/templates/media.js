834ef99451897ada83d834610b8ddbfae346fd7a
Lu1l <92431867+Lu1l@users.noreply.github.com>
2024-09-13 16:55:25 +0200

    structure amélioré requete en preparation

--- scripts/templates/media.js
+++ scripts/templates/media.js
@@ -6,20 +6,34 @@ function mediaTemplate(data, result) {
     const picture = `assets/photographers/${fullname[0]}/${image}`;
 
     function getMediaCardDOM() {
-
         const article = document.createElement( 'article' );  
-        const card_div = document.createElement( 'div' );
-        pgPrice.classList.add("display");
         const img = document.createElement( 'img' );
+        img.classList.add("set");
         img.setAttribute("src", picture)
         const p = document.createElement( 'p' );   
         p.textContent = title;
         article.appendChild(img);
         article.appendChild(p);
-        
         return (article);
 
-       
+       /*
+       const article = document.createElement( 'article' ); 
+       const display = document.createElement( 'div' );
+       display.classList.add("display");
+       const media = document.createElement( 'div' );
+       media.classList.add("card");
+       const display = document.createElement( 'div' );
+       media-img.classList.add("media-img");
+       const img = document.createElement( 'img' );
+       img.setAttribute("src", picture)
+       const name_div = document.createElement( 'div' );
+       name_div.classList.add("name");
+       const title = document.createElement( 'p' );
+       const like = document.createElement( 'p' );
+       const heart = document.createElement( 'i' );
+       heart.classList.add("fa");
+       heart.classList.add("fa-heart");
+        */
             
     }
     return {getMediaCardDOM}
