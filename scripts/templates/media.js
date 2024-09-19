19936a0bc896a901165d1fd30f246b8e82062373
Lu1l <92431867+Lu1l@users.noreply.github.com>
2024-09-19 19:54:02 +0200

    integration de la video, reste a créer le css et implémenté le filtre

--- scripts/templates/media.js
+++ scripts/templates/media.js
@@ -1,22 +1,44 @@
 function mediaTemplate(data, result) {
-    const { title, image, likes, photographerId, date } = data;
+    const { title, image, likes, photographerId, date,video } = data;
     const { name } = result;
     const fullname = name.split(" ");
-    console.log(title)
-    const picture = `assets/photographers/${fullname[0]}/${image}`;
+   
 
+
+    
     function getMediaCardDOM() {
-        const article = document.createElement( 'article' );  
-        const img = document.createElement( 'img' );
-        img.classList.add("set");
-        img.setAttribute("src", picture)
+        const article = document.createElement( 'article' ); 
+        if(video){
+            const ctr_video = document.createElement( 'video' );
+            ctr_video.controls = true;
+            const src = `assets/photographers/${fullname[0]}/${video}`;
+            const media_video = document.createElement( 'source' );
+            media_video.classList.add("set");
+            media_video.setAttribute("src", src)
+            
+            ctr_video.appendChild(media_video);
+            article.appendChild(ctr_video)
+            //creer un element video 
+            //creer les attributs
+            //créer une source une source 
+            //créer les attributs
+            //mettre la source dans la video
+            //mettre la video dans l'article
+        } 
+      
+        if(image){
+            const src = `assets/photographers/${fullname[0]}/${image}`;
+            const img = document.createElement( 'img' );
+            img.classList.add("set");
+            img.setAttribute("src", src)
+            article.appendChild(img);
+        }
+    
         const p = document.createElement( 'p' );   
         p.textContent = title;
-        article.appendChild(img);
         article.appendChild(p);
-        return (article);
-
-       /*
+       
+      /*
        const article = document.createElement( 'article' ); 
        const display = document.createElement( 'div' );
        display.classList.add("display");
@@ -31,9 +53,9 @@ function mediaTemplate(data, result) {
        const title = document.createElement( 'p' );
        const like = document.createElement( 'p' );
        const heart = document.createElement( 'i' );
-       heart.classList.add("fa");
-       heart.classList.add("fa-heart");
-        */
+       heart.classList.add("fa","fa-heart");
+       */
+     return (article);
             
     }
     return {getMediaCardDOM}
